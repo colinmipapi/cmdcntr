889d7a041dcc882db892b3b2e85a498dd06c12d1
colinmipapi <colinjmcfaul@gmail.com>
2017-11-29 22:34:09 -0500

    Added EB database config and requirements files

--- cmdcntr/settings.py
+++ cmdcntr/settings.py
@@ -121,9 +121,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, "www", "static")
 STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
-StaticFiles = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 AUTH_USER_MODEL = 'base.User'
 ACCOUNT_ACTIVATION_DAYS = 7
