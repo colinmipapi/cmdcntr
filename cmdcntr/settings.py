227275e48f0dd20c7d9da18db9bc63ecae24415b
colinmipapi <colinjmcfaul@gmail.com>
2017-11-29 20:25:27 -0500

    fixed allowed hosts

--- cmdcntr/settings.py
+++ cmdcntr/settings.py
@@ -15,7 +15,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cmdcntr-dev.us-east-1.elasticbeanstalk.com','127.0.0.1:8000','colinmcfaul.com']
 
 
 # Application definition
