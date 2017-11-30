65f2f474a55261256737a153bbf76154e4f9fd0f
colinmipapi <colinjmcfaul@gmail.com>
2017-11-29 21:07:03 -0500

    collectstatic done

--- cmdcntr/settings.py
+++ cmdcntr/settings.py
@@ -118,12 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
 
 AUTH_USER_MODEL = 'base.User'
 ACCOUNT_ACTIVATION_DAYS = 7
