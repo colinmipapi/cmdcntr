b3267ec29736f26040de711f8fa07edd154a74d0
colinmipapi <colinjmcfaul@gmail.com>
2017-11-29 20:20:33 -0500

    add secret_settings

---
+++ cmdcntr/secret_settings.py
@@ -0,0 +1,4 @@
+SECRET_KEY = 'v--p_n#rb%x#10u(-12yg_wg==unz@(thnkou6=w0*a=+5qv=!'
+TWILIO_KEY = '6a2a994a5187dfb717f31fc03fa3a652'
+AWS_ACCESS_KEY = 'AKIAI5UMIFCGBDV2ELUA'
+AWS_SECRET_KEY = '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'
