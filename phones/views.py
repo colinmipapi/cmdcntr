dcac6ace8e982ccdc3ce55148edd894c6e799f3c
colinmipapi <colinjmcfaul@gmail.com>
2017-11-30 10:14:23 -0500

    added sms response

--- phones/views.py
+++ phones/views.py
@@ -4,8 +4,20 @@ from django.http import HttpResponse
 
 from twilio.twiml.messaging_response import MessagingResponse
 
+def load_twilio_config():
+    twilio_account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
+    twilio_auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
+    twilio_number = os.environ.get('TWILIO_NUMBER')
+
+    if not all([twilio_account_sid, twilio_auth_token, twilio_number]):
+        logger.error(NOT_CONFIGURED_MESSAGE)
+        raise MiddlewareNotUsed
+
+    return (twilio_number, twilio_account_sid, twilio_auth_token)
+
+
 @csrf_exempt
 def sms(request):
     r = MessagingResponse()
     r.message('Hello from your Django app!')
-    return HttpResponse(r.toxml(), content_type='text/xml')
+    return HttpResponse(str(r))
