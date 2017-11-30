98e624662e7241c88561f94752cbdd78367e5f56
colinmipapi <colinjmcfaul@gmail.com>
2017-11-29 21:45:55 -0500

    Added EB database config and requirements files

--- cmdcntr/settings.py
+++ cmdcntr/settings.py
@@ -119,8 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_ROOT = 'static'
-STATIC_URL = '/static/=static/'
+STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
+StaticFiles = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 AUTH_USER_MODEL = 'base.User'
 ACCOUNT_ACTIVATION_DAYS = 7
